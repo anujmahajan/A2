76f815164fb4772f88c3d20b7f65632d4f4fcabe
ApoorvaSuneel <apoorvasuneel3@gamil.com>
2016-03-29 05:04:59 +0530

    without polls and threads

--- app/src/main/java/com/example/user/complaintapp/ResolveD.java
+++ app/src/main/java/com/example/user/complaintapp/ResolveD.java
@@ -7,6 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
@@ -14,9 +26,13 @@ import android.widget.Button;
  */
 public class ResolveD extends Fragment {
 
-     Button b1,b2,b3,b4,b5,b6;
+    Button b1,b2,b3,b4,b5,b6;
+    private TextView t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13;
+    private String i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13;
+    private String JSON1,JSON2,JSON3,JSON4,JSON5,JSON6 ;
     public ResolveD() {
         // Required empty public constructor
+
     }
 
 
@@ -24,50 +40,67 @@ public class ResolveD extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resolve_d, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_resolve_d, container, false);
+        t1 = (TextView) v.findViewById(R.id.cv1);
+        t2 = (TextView) v.findViewById(R.id.cv2);
+        t3 = (TextView) v.findViewById(R.id.cv3);
+        t4 = (TextView) v.findViewById(R.id.cv4);
+        t5 = (TextView) v.findViewById(R.id.cv5);
+        t6 = (TextView) v.findViewById(R.id.cv6);
+        t7 = (TextView) v.findViewById(R.id.cv7);
+        t8 = (TextView) v.findViewById(R.id.cv8);
+        t9 = (TextView) v.findViewById(R.id.cv9);
+        t10 = (TextView) v.findViewById(R.id.cv10);
+        t11 = (TextView) v.findViewById(R.id.cv11);
+        t12 = (TextView) v.findViewById(R.id.cv12);
+        t13 = (TextView) v.findViewById(R.id.cv13);
+        b1 = (Button) v.findViewById(R.id.b1);
+        b2 = (Button) v.findViewById(R.id.b2);
+        b3 = (Button) v.findViewById(R.id.b3);
+        b4 = (Button) v.findViewById(R.id.b4);
+        b5 = (Button) v.findViewById(R.id.b5);
+        b6 = (Button) v.findViewById(R.id.b6);
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            i1 = bundle.getString("txt");
+            i2 = bundle.getString("id");
+            i3 = bundle.getString("prio");
+            i4 = bundle.getString("comp");
+            i5 = bundle.getString("poll");
+            i6 = bundle.getString("res");
+            i7 = bundle.getString("thread");
+            i8 = bundle.getString("anon");
+            i9 = bundle.getString("status");
+            i10 = bundle.getString("teamid");
+            i11 = bundle.getString("imageid");
+            i12 = bundle.getString("red");
+            i13 = bundle.getString("pers");
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        b1=(Button)getActivity().findViewById(R.id.status);
-        b2=(Button)getActivity().findViewById(R.id.poll);
-        b3=(Button)getActivity().findViewById(R.id.redun);
-        b4=(Button)getActivity().findViewById(R.id.up);
-        b5=(Button)getActivity().findViewById(R.id.down);
-        b6=(Button)getActivity().findViewById(R.id.thread);
-        //check status
-        b1.setOnClickListener(new View.OnClickListener() {
+            t1.setText(i1);
+            t2.setText(i2);
+            t3.setText(i3);
+            t4.setText(i4);
+            t5.setText(i5);
+            t6.setText(i6);
+            t7.setText(i7);
+            t8.setText(i8);
+            t9.setText(i9);
+            t10.setText(i10);
+            t11.setText(i11);
+            t12.setText(i12);
+            t13.setText(i13);
+        }
 
-            public void onClick(View v) {
-
-             checkstatus();
-            }
-        });
-        //set up poll
-        b2.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-             setpoll();
-
-            }
-        });
-       //mark redundant
-        b3.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-             redundant();
-            }
-        });
-        //upvote
+        JSON1 = Login.ip + "default/upvotec.json/" + i2 + "?up=1";//upvote
+        JSON2 = Login.ip + "default/upvotec.json/" + i2 + "?up=0";//downvote
+        JSON3 = Login.ip + "markred.json/" + i2;//mark red
         b4.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-             upvote();
+                upvote();
+
             }
         });
-         //downvote
         b5.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -75,37 +108,111 @@ public class ResolveD extends Fragment {
 
             }
         });
-        //view thread
         b6.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-             viewthread();
-
+                markred();
             }
         });
 
 
-
+        return v;
     }
 
-    //marks complaint redundant
-    private void redundant() {
-
-    }
-    //upvotes a complaint
     private void upvote() {
+
+        JsonObjectRequest jreq = new JsonObjectRequest(Request.Method.GET,
+                JSON1, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+
+                try {
+                    // Parsing json object response
+                    Boolean b=response.getBoolean("success");
+                    if(b)
+                    {
+
+                        Toast.makeText(getActivity(),
+                                "Upvoted",
+                                Toast.LENGTH_SHORT).show();
+                    }}
+                catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(),
+                            "Error: NOT WORKING " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }}}, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }});
+        RequestQueue RequestP = Volley.newRequestQueue(getActivity());
+        RequestP.add(jreq);
+
     }
-    //downvotes a complaint
     private void downvote() {
+        JsonObjectRequest jreq = new JsonObjectRequest(Request.Method.GET,
+                JSON2, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+
+                try {
+                    // Parsing json object response
+                    Boolean b=response.getBoolean("success");
+                    if(b)
+                    {
+
+                        Toast.makeText(getActivity(),
+                                "DownVoted",
+                                Toast.LENGTH_SHORT).show();
+                    }}
+                catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(),
+                            "Error: NOT WORKING " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }}}, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }});
+        RequestQueue RequestP = Volley.newRequestQueue(getActivity());
+        RequestP.add(jreq);
+
+
+    }
+    private void markred() {
+        JsonObjectRequest jreq = new JsonObjectRequest(Request.Method.GET,
+                JSON3, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+
+                try {
+                    // Parsing json object response
+                    Boolean b=response.getBoolean("success");
+                    if(b)
+                    {
+
+                        Toast.makeText(getActivity(),
+                                "Marked Redundant",
+                                Toast.LENGTH_SHORT).show();
+                    }}
+                catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(),
+                            "Error: NOT WORKING " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }}}, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }});
+        RequestQueue RequestP = Volley.newRequestQueue(getActivity());
+        RequestP.add(jreq);
+
     }
 
-    //views a thread
-    private void viewthread() {
-    }
-    //sets up a poll
-    private void setpoll() {
-    }
-    //check status
-    private void checkstatus() {
-    }
 }
