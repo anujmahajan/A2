7cf3af6ef090fd175ef8645c960e28fe8f46d0d3
ApoorvaSuneel <apoorvasuneel3@gamil.com>
2016-03-29 11:45:19 +0530

    final addition of fragments

---
+++ app/src/main/java/com/example/user/complaintapp/ThreadView.java
@@ -0,0 +1,29 @@
+package com.example.user.complaintapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ThreadView extends Fragment {
+
+
+    public ThreadView() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_thread_view, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/user/complaintapp/search.java
@@ -0,0 +1,30 @@
+package com.example.user.complaintapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class search extends Fragment {
+
+
+    public search() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+
+
+}
